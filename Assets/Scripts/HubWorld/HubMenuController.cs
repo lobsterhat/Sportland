6dc78dc8a4595f4f71106df060007e7da9b90734
DESKTOP-9TKFRBS\epilnock <epinock@8bitdev.com>
2026-01-02 01:40:44 -0700

    Update

---
+++ Assets/Scripts/HubWorld/HubMenuController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Sportland.Core.GameManagement;
+using Sportland.Core.Athletes;
+
+namespace Sportland.UI.Hub
+{
+    public class HubMenuController : MonoBehaviour
+    {
+        public Button playBasketballButton;
+
+        private void Start()
+        {
+            // Hook up button click
+            if (playBasketballButton != null)
+            {
+                playBasketballButton.onClick.AddListener(OnPlayBasketballClicked);
+            }
+        }
+
+        private void OnPlayBasketballClicked()
+        {
+            Debug.Log("Play Basketball clicked!");
+
+            // Create a simple game context
+            GameContext context = new GameContext
+            {
+                gameDate = CoreGameManager.Instance.currentDate,
+                homeRoster = CoreGameManager.Instance.GetAllAthletes(),
+                awayRoster = CoreGameManager.Instance.GetAllAthletes(),
+                isPlayoffGame = false,
+                venueName = "Test Arena"
+            };
+
+            // Load basketball!
+            CoreGameManager.Instance.LoadSport(SportType.Basketball, context);
+        }
+    }
+}
